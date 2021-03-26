8f01ce156bac0a70fcca3caef63a555a928abd9f
Jorge Rueda <jorgeru045@gmail.com>
2021-03-26 15:12:06 -0500

    Adicion Django Admin incluyendo TabularInline

--- pruebamonet/monet/admin.py
+++ pruebamonet/monet/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Detalle, Control
+
+class DetalleInLine(admin.TabularInline):
+    model = Detalle
+    extra = 3
+
+
+class ControlAdmin(admin.ModelAdmin):
+    inlines = [DetalleInLine]
+
+
+admin.site.register(Control, ControlAdmin)
