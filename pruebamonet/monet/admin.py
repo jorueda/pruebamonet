fdb1869ad429b379fdeb7736dd6ef6e4301daea8
Jorge Rueda <jorgeru045@gmail.com>
2021-03-27 11:19:32 -0500

    Mejora Django admin

--- pruebamonet/monet/admin.py
+++ pruebamonet/monet/admin.py
@@ -5,12 +5,14 @@ from .models import Detalle, Control, Archivo
 
 class DetalleInLine(admin.TabularInline):
     model = Detalle
-    extra = 3
+    extra = 1
 
 
 class ControlAdmin(admin.ModelAdmin):
     inlines = [DetalleInLine]
-
+    list_display = ('nit_entidad', 'fecha_transmision', 'numero_registros', 'valor_total_transacciones')
+    list_filter = ('nit_entidad', 'fecha_transmision', 'numero_registros', 'valor_total_transacciones')
+    search_fields = ('nit_entidad', 'fecha_transmision', 'valor_total_transacciones')
 
 admin.site.register(Control, ControlAdmin)
 admin.site.register(Archivo)
